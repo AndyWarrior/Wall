019a03965bb35b1155fd153e8d0fd864cfd0e9dd
Andres Lopez De Leon <agldeleon@gmail.com>
2016-03-08 13:31:28 -0600

    Create basic structure

---
+++ Assets/Scripts/MoveWall.js
@@ -0,0 +1,17 @@
+﻿public static var speed : float;
+private var pos : float;
+ 
+ function Start () {
+  speed = 1;
+  pos = Random.Range (0, 3.6);
+  transform.position.x = pos - 1.80;
+ }
+ 
+ function Update () {
+  pos += Time.deltaTime * speed;
+  transform.position.x = Mathf.PingPong (pos, 3.6) - 1.8;
+  if (Input.GetMouseButtonDown(0))
+   {
+      Destroy(this);
+   }
+ }
---
+++ Assets/Scripts/StopWall.js
@@ -0,0 +1,9 @@
+﻿public var Square: Transform;
+function Update()
+ {
+   if (Input.GetMouseButtonDown(0))
+   {
+      var go = Instantiate(Square, new Vector2(0,0), Quaternion.identity);
+      go.gameObject.AddComponent(MoveWall);
+   }
+ }
